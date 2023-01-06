3b6d6476ae1bd4d5f2d5079f0e0ed29f5c189d24
Sahiba Kumari <sahibakumari0915@outlook.com>
2023-01-06 20:06:54 +0530

    style: added hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React from 'react';
+import Helmet from '../components/Helmet/Helmet';
+import {Container, Row, Col} from "reactstrap";
+import heroImg from '../assets/images/hero-img.png';
+import '../styles/home.css';
 
 const Home = () => {
+
+  const year = new Date().getFullYear();
   return (
-    <div>Home</div>
+    <Helmet title = {'Home'}>
+      <section className="hero-section">
+        <Container>
+          <Row>
+            <Col lg='6' md='6'>
+              <div className="hero-content">
+                <p className="hero-subtitle">
+                  Trending Product in {year}
+                </p>
+                <h2>Make your products more Minimalistic and Modern</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad voluptate, aperiam voluptatibus et doloremque explicabo optio cupiditate nulla architecto doloribus asperiores
+                   laborum temporibus perferendis distinctio totam amet tenetur! Necessitatibus, sapiente!
+                </p>
+                <button className="buy-btn">Shop Now</button>
+              </div>
+            </Col>
+
+            <Col lg='6' md='6'>
+              <div className="hero-img">
+                <img src={heroImg} alt="hero-img" />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
   )
 }
 

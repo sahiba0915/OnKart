6a1af1ff3998878bc468b2835716416ed868ae77
Sahiba Kumari <sahibakumari0915@outlook.com>
2023-01-20 21:02:37 +0530

    added reviews section

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React ,  {useState} from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import { useParams } from 'react-router-dom';
 import products from '../assets/data/products';
@@ -9,14 +9,14 @@ import { motion } from 'framer-motion';
 
 
 const ProductDetails = () => {
-
+  const[tab, setTab] = useState('desc');
   const {id}= useParams();
   const product = products.find((item)=>item.id===id);
-  const{imgUrl, productName, price, avgRating, shortDesc, review, description } = product;
+  const{imgUrl, productName, price, avgRating, shortDesc, reviews, description } = product;
 
   return (
-    <Helmet>
-     <CommonSection/>
+    <Helmet title={productName}>
+     <CommonSection title={productName}/>
      <section className='pt-0'>
       <Container>
         <Row>
@@ -51,6 +51,42 @@ const ProductDetails = () => {
 
             <motion.button whileTap={{scale: 1.2}} className="buy-btn">Add to Cart</motion.button>
             </div>
+          </Col>
+        </Row>
+      </Container>
+     </section>
+
+     <section>
+      <Container>
+        <Row>
+          <Col lg='12'>
+            <div className="tab-wrapper d-flex align-items-center gap-5">
+              <h6 className={`${tab==='desc' ? 'active-tab' : ''}`} onClick={()=>setTab('desc')}>Description</h6>
+              <h6 className={`${tab==='rev' ? 'active-tab' : ''}`} onClick={()=>setTab('rev')}>Reviews ({reviews.length})</h6>
+            </div>
+
+            {
+              tab === 'desc' ? ( <div className="tab-content mt-5">
+              <p>{description}</p>
+            </div> ) : (
+            <div className='product-review mt-5'>
+              <div className="review-wrapper">
+                <ul>
+                  {
+                    reviews?.map((item, index)=>(
+                      <li key={index}>
+                        <span>{item.rating}( ratings)</span>
+                        <p className='mt-2 mb-2'>{item.text}</p>
+                      </li>
+                    ))
+                  }
+                </ul>
+              </div>
+              </div>
+            )
+            }
+
+           
           </Col>
         </Row>
       </Container>

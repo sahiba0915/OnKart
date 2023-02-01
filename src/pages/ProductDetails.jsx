bfac5c4b033d8f415bc6bac11269412bcf1f4714
Sahiba Kumari <sahibakumari0915@outlook.com>
2023-02-01 13:01:03 +0530

    completed products page

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,4 +1,4 @@
-import React ,  {useState} from 'react';
+import React ,  {useState, useRef, useEffect} from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import { useParams } from 'react-router-dom';
 import products from '../assets/data/products';
@@ -6,13 +6,54 @@ import Helmet from '../components/Helmet/Helmet';
 import CommonSection from '../components/UI/CommonSection';
 import'../styles/product-details.css';
 import { motion } from 'framer-motion';
+import ProductList from '../components/UI/ProductList';
+import { useDispatch } from 'react-redux';
+import {cartActions} from '../redux/slices/cartSlice';
+import { toast } from 'react-toastify';
 
 
 const ProductDetails = () => {
   const[tab, setTab] = useState('desc');
+  const[rating, setRating] = useState(null);
+  const reviewUser = useRef('');
+  const reviewMsg = useRef('');
+  const dispatch = useDispatch();
   const {id}= useParams();
   const product = products.find((item)=>item.id===id);
-  const{imgUrl, productName, price, avgRating, shortDesc, reviews, description } = product;
+  const{imgUrl, productName, price, avgRating, shortDesc, reviews, description, category } = product;
+
+  const relatedProducts = products.filter(item => item.category === category);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const reviewUserName = reviewUser.current.value;
+    const reviewUserMsg = reviewMsg.current.value;
+
+    const reviewObj = {
+      userName : reviewUserName,
+      userMsg : reviewUserMsg,
+      rating,
+    }
+
+    console.log(reviewObj);
+    toast.success("Review submitted");
+  };
+
+  const addToCart = () => {
+    dispatch(cartActions.addItem({
+      id,
+      image: imgUrl,
+      productName,
+      price,
+    })
+);
+toast.success("Product Added Successfully");
+  }
+
+  useEffect(()=>{
+       window.scrollTo(0,0);
+  },[product])
 
   return (
     <Helmet title={productName}>
@@ -46,10 +87,14 @@ const ProductDetails = () => {
               </div>
               <p>(<span>{avgRating}</span> ratings)</p>
             </div>
+            <div className='d-flex align-items-center gap-5'>
             <span className='product-price'>${price}</span>
+            <span>Category: {category.toUpperCase()}</span>
+            </div>
+            
             <p className='mt-3'>{shortDesc}</p>
 
-            <motion.button whileTap={{scale: 1.2}} className="buy-btn">Add to Cart</motion.button>
+            <motion.button whileTap={{scale: 1.2}} className="buy-btn" onClick={addToCart}>Add to Cart</motion.button>
             </div>
           </Col>
         </Row>
@@ -74,20 +119,49 @@ const ProductDetails = () => {
                 <ul>
                   {
                     reviews?.map((item, index)=>(
-                      <li key={index}>
+                      <li key={index} className="mb-4">
+                        <h6>John Doe</h6>
                         <span>{item.rating}( ratings)</span>
                         <p className='mt-2 mb-2'>{item.text}</p>
                       </li>
                     ))
                   }
                 </ul>
+
+                <div className="review-form">
+                  <h4>Leave your experience</h4>
+                  <form action="" onSubmit={submitHandler}>
+                    <div className="form-group">
+                      <input type="text" placeholder='Enter Name' ref={reviewUser} required/>
+                    </div>
+
+                    <div className="form-group d-flex align-items-center gap-5 rating-group">
+                      <motion.span whileTap={{scale: 1.2}} onClick={()=>setRating(1)}> 1<i class="ri-star-s-fill"></i></motion.span>
+                      <motion.span whileTap={{scale: 1.2}} onClick={()=>setRating(2)}>2<i class="ri-star-s-fill"></i></motion.span>
+                      <motion.span whileTap={{scale: 1.2}} onClick={()=>setRating(3)}>3<i class="ri-star-s-fill"></i></motion.span>
+                      <motion.span whileTap={{scale: 1.2}} onClick={()=>setRating(4)}>4<i class="ri-star-s-fill"></i></motion.span>
+                      <motion.span whileTap={{scale: 1.2}} onClick={()=>setRating(5)}>5<i class="ri-star-s-fill"></i></motion.span>
+                    </div>
+
+                    <div className="form-group">
+                      <textarea rows={4} type="text" placeholder='Review Message.......' ref={reviewMsg} required/>
+                    </div>
+
+                    <motion.button whileTap={{scale: 1.2}} type="submit" className='buy-btn'>Submit</motion.button>
+
+                  </form>
+
+                </div>
               </div>
               </div>
             )
-            }
-
-           
+            }           
           </Col>
+
+          <Col lg='12' className='mt-5'>
+            <h2 className="related-title">You might also like</h2>
+          </Col>
+          <ProductList data={relatedProducts}/>
         </Row>
       </Container>
      </section>
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -68,7 +68,7 @@ const Shop = () => {
       <section>
         <Container>
           <Row>
-            <Col lg='3' md='3'>
+            <Col lg='3' md='6'>
               <div className="filter-widget">
                 <select onChange={handleFilter}>
                 <option>Filter by Category</option>
@@ -80,7 +80,7 @@ const Shop = () => {
                 </select>
                 </div>
             </Col>
-            <Col lg='3' md='3'>
+            <Col lg='3' md='6' className='text-end'>
             <div className="filter-widget">
             <select>
                 <option>Sort by</option>
@@ -89,7 +89,7 @@ const Shop = () => {
             </select>
             </div>
             </Col>
-            <Col lg='6' md='6'>
+            <Col lg='6' md='12'>
               <div className="search-box">
                 <input type="text" placeholder='Search.....' onChange={handleSearch}/>
                 <span><i class="ri-search-line"></i></span>
